0281fa801d52a6b51da8e95b9f109a19d02b8673
AleksandrKotenok <83491711+AleksandrKotenok@users.noreply.github.com>
2021-12-28 14:47:40 +0200

    init

---
+++ src/components/AddForm/AddForm.jsx
@@ -0,0 +1,53 @@
+import { Component } from "react";
+import s from "../AddForm/AddForm.module.css";
+
+export class AddForm extends Component {
+  state = {
+    name: "",
+    number: "",
+  };
+
+  inputChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.props.submit(this.state);
+    this.setState({ name: "", number: "" });
+  };
+
+  render() {
+    return (
+      <section className={s.addForm}>
+        <form className={s.form} onSubmit={this.onSubmit}>
+          <label className={s.label}>Name:</label>
+          <input
+            className={s.input}
+            type="text"
+            name="name"
+            value={this.state.name}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.inputChange}
+          />
+          <label className={s.label}>Tel:</label>
+          <input
+            className={s.input}
+            type="tel"
+            name="number"
+            value={this.state.number}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            onChange={this.inputChange}
+          />
+          <button className={s.button} type="submit">
+            Add contact
+          </button>
+        </form>
+      </section>
+    );
+  }
+}
---
+++ src/components/App/App.jsx
@@ -0,0 +1,64 @@
+import { AddForm } from "../AddForm/AddForm";
+import { Filter } from "../Filter/Filter";
+import { ContactList } from "../ContactList/ContactList";
+import React, { Component, Fragment } from "react";
+import { nanoid } from "nanoid";
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: "",
+  };
+  #contacts = "contacts";
+  componentDidMount() {
+    const contactsFromLs = JSON.parse(localStorage.getItem(this.#contacts));
+    if (contactsFromLs) this.setState({ contacts: contactsFromLs });
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem(this.#contacts, JSON.stringify(this.state.contacts));
+    }
+  }
+  submit = ({ name, number }) => {
+    const existingName = this.state.contacts.find((contact) => contact.name === name);
+    if (existingName) {
+      alert(`${name} existing name`);
+      return;
+    }
+    const newRecord = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    this.setState(({ contacts }) => ({
+      contacts: [newRecord, ...contacts],
+    }));
+  };
+  filterContacts = () => {
+    return this.state.contacts.filter((contact) => contact.name.toLowerCase().includes(this.state.filter.toLowerCase()));
+  };
+  deleteContact = (contactId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.id !== contactId),
+    }));
+  };
+  filterChange = (e) => {
+    this.setState({
+      filter: e.currentTarget.value,
+    });
+  };
+
+  render() {
+    const filteredContacts = this.filterContacts();
+    return (
+      <Fragment>
+        <h1>Phonebook</h1>
+        <AddForm submit={this.submit} />
+        <h2>Contacts</h2>
+        <Filter filter={this.state.filter} input={this.filterChange} />
+        <ContactList contacts={filteredContacts} deleteContact={this.deleteContact} />
+      </Fragment>
+    );
+  }
+}
+export default App;
